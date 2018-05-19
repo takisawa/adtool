e8f4fe57c5779591bb5bf43b5920c3b09f8fd62a
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-05-19 20:12:34 +0900

    Add calc_ecpm function.

--- adtool.py
+++ adtool.py
@@ -4,7 +4,13 @@ def calc_ctr(imp, click):
     return click / imp
 
 
-imp = 30000 # 30万IMP
-click = 100
+def calc_ecpm(imp, click, cpc):
+    ctr = calc_ctr(imp, click)
+    return ctr * cpc * 1000
+
+imp = 50000 # 30万IMP
+click = 200
+cpc = 30
 
 print("CTC: %f" % calc_ctr(imp, click))
+print("eCPM: %f" % calc_ecpm(imp, click, cpc))
