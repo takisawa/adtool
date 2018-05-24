ff584f723687e2eeac8924fc3616a87773edb1ee
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-05-25 06:35:33 +0900

    Add calc_cvr method.

--- adtool.py
+++ adtool.py
@@ -4,6 +4,9 @@ class AdTool():
     def calc_ctr(self, imp, click):
         return click / imp
 
+    def calc_cvr(self, click, conversion):
+        return conversion / click
+
     def calc_ecpm(self, imp, click, cpc):
         ctr = self.calc_ctr(imp, click)
         return ctr * cpc * 1000
@@ -13,6 +16,7 @@ class AdTool():
 imp = 50000 # 30万IMP
 click = 200
 cpc = 30
+conversion = 2
 
 
 adtool = AdTool()
@@ -20,3 +24,4 @@ adtool = AdTool()
 ctr = adtool.calc_ctr(imp, click)
 print("CTC: %f (%.2f%%)" % (ctr, ctr*100))
 print("eCPM: %f" % adtool.calc_ecpm(imp, click, cpc))
+print("CVR: %f" % adtool.calc_cvr(click, conversion))
