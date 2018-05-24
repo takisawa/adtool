e0e37a08d788147bfc2c8c3b71c6cc68d1ae3acc
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-05-24 22:25:01 +0900

    Add AdTool Class.

--- adtool.py
+++ adtool.py
@@ -1,17 +1,22 @@
 
+class AdTool():
 
-def calc_ctr(imp, click):
-    return click / imp
+    def calc_ctr(self, imp, click):
+        return click / imp
+
+    def calc_ecpm(self, imp, click, cpc):
+        ctr = self.calc_ctr(imp, click)
+        return ctr * cpc * 1000
 
 
-def calc_ecpm(imp, click, cpc):
-    ctr = calc_ctr(imp, click)
-    return ctr * cpc * 1000
 
 imp = 50000 # 30万IMP
 click = 200
 cpc = 30
 
-ctr = calc_ctr(imp, click)
+
+adtool = AdTool()
+
+ctr = adtool.calc_ctr(imp, click)
 print("CTC: %f (%.2f%%)" % (ctr, ctr*100))
-print("eCPM: %f" % calc_ecpm(imp, click, cpc))
+print("eCPM: %f" % adtool.calc_ecpm(imp, click, cpc))
